0abbc1978793bb76b731eb1b00688b6fadd2ddde
Luis Guilherme <luiguinm@gmail.com>
2025-03-17 17:14:48 -0300

    Condições pt.1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro foi de ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log('Ultrapassastes a velocidade permitida, MULTADO!!!!!!')
+}
+console.log('Dirija sempre usando o cinto de segurança.')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Nascido em ${país}`)
+if (país == 'Brasil') {
+    console.log('tu é um Neguim tupiniquim')
+} else {
+    console.log('Estrangeiro >:(')
+}
