c13bf21392f9c907d92132cc7de220398e9f65fd
Luis Guilherme <luiguinm@gmail.com>
2025-03-20 10:05:19 -0300

    estruturas de repetição no JavaScript

---
+++ aula13/ex016.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`começou ${c}`)
+    c++
+} while (c <= 6) 
+
+
+
+/* var c = 1
+while (c <= 6) {
+    console.log(`começou ${c}`)
+    c++
+} */
---
+++ aula14/ex017.js
@@ -0,0 +1,5 @@
+console.log('has the memory gone?')
+for(var c = 1; c <= 5; c++) {
+    console.log(`discourage by the noise ${c}`)
+}
+console.log('are you feeling numb?')
